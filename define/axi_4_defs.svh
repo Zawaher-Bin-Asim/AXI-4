36f2508440b3646a6eea363e13a197cf87fa7471
Zawaher-Bin-Asim <zawaherbinasim.333@gmail.com>
2025-05-28 03:02:26 +0500

    Updated for the compilation error

--- define/axi_4_defs.svh
+++ define/axi_4_defs.svh
@@ -1,3 +1,7 @@
+//Author        : Zawaher Bin Asim , UET Lahore
+//Description   : This the header file  for  axi 4 
+// Date         : 15 Sep, 2024.
+
 `ifndef axi_4_defs
 `define axi_4_defs
 
@@ -5,41 +9,17 @@
 `define DATA_BUS_WIDTH  512
 `define MEM_DEPTH       4096
 
-parameter BURST_MAX = 256;  // or 256 if max len = 255
-parameter STROBE_WIDTH = (`DATA_BUS_WIDTH/8);
-
-typedef enum logic [2:0]{  
-    MASTER_IDLE,
-    WAIT_ARREADY,
-    WAIT_RVALID,
-    WAIT_AWREADY_WREADY,
-    WAIT_AWREADY,
-    WAIT_WREADY,
-    WAIT_WLAST,
-    WAIT_BVALID
-} axi_4_master_states_e;
-
-typedef enum logic [3:0]{  
-    SLAVE_IDLE,
-    WAIT_ARVALID,
-    DATA_FETCH,
-    WAIT_RREADY,
-    WAIT_AWVALID_WVALID,
-    WAIT_AWVALID,
-    WAIT_WVALID,
-    DATA_STORE,
-    WAIT_BREADY
-} axi_4_slave_states_e;
-
+`define BURST_MAX  16  // or 256 if max len = 255
+`define STROBE_WIDTH  (`DATA_BUS_WIDTH/8)
 
 // Parameters for burst type
-parameter BURST_FIXED = 2'b00;
-parameter BURST_INCR  = 2'b01;
-parameter BURST_WRAP  = 2'b10;
+`define BURST_FIXED  2'b00
+`define BURST_INCR   2'b01
+`define BURST_WRAP   2'b10
 
 // Parameters for the response 
-parameter RESP_OKAY   = 2'b00;
-parameter RESP_SLVERR = 2'b10;
-parameter RESP_DECERR = 2'b11;
+`define RESP_OKAY    2'b00
+`define RESP_SLVERR  2'b10
+`define RESP_DECERR  2'b11
 
 `endif
--- define/axi_4_pkg.sv
+++ define/axi_4_pkg.sv
@@ -4,6 +4,29 @@ package axi_4_pkg;
     parameter DATA_BUS_WIDTH = 512;
     parameter STROBE_WIDTH  = DATA_BUS_WIDTH/8;
     
+        typedef enum logic [2:0]{  
+        MASTER_IDLE,
+        WAIT_ARREADY,
+        WAIT_RVALID,
+        WAIT_AWREADY_WREADY,
+        WAIT_AWREADY,
+        WAIT_WREADY,
+        WAIT_WLAST,
+        WAIT_BVALID
+    } axi_4_master_states_e;
+
+    typedef enum logic [3:0]{  
+        SLAVE_IDLE,
+        WAIT_ARVALID,
+        DATA_FETCH,
+        WAIT_RREADY,
+        WAIT_AWVALID_WVALID,
+        WAIT_AWVALID,
+        WAIT_WVALID,
+        DATA_STORE,
+        WAIT_BREADY
+    } axi_4_slave_states_e;
+
     typedef struct {
     logic   [XLEN-1:0] arid;       // read address transaction id
     logic   [XLEN-1:0] awid;       // write address transction id
